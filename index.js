94e751af8fa41744e93229782d5e6627332ddf7b
neo <kremersneo0@gmail.com>
2024-06-29 15:44:50 +0200

    Hab gefühlt alles anders gemacht

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require("express")
 const fs = require("fs")
 
 const app = express()
-const port = 3000
+const port = 3100
 
 app.get("/background", (req, res) => {
     res.send(fs.readFileSync("faser_logo_long.png"))
+})
+
+app.get("/login", (req, res) => {
+    res.send(fs.readFileSync("login.html", "utf8"))
+})
+
+app.get("/register", (req, res) => {
+    res.send(fs.readFileSync("register.html", "utf8"))
+})
+
+app.get("/account", (req, res) => {
+    res.send(fs.readFileSync("account.html", "utf8"))
 })
 
 app.get("/*", (req, res) => {
